e06d8c7c96bdc6a9fec38bcb1ab7d028ec4cad84
Taehwan An <conscious52th@gmail.com>
2025-02-24 05:52:28 +0900

    adding time sleep

--- one_off_dyno_test.py
+++ one_off_dyno_test.py
@@ -1,7 +1,10 @@
 import requests
+import time
 
 def request_test():
   print("Hello")
+  time.sleep(300)
+
   res = requests.get(url="https://my-development-app-6482cbf3b808.herokuapp.com/")
   print(res)
 
