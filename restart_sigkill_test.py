e6da9a84d1b6553c7fd19f2c51c48b21e36d8bca
Taehwan An <conscious52th@gmail.com>
2025-02-24 09:20:32 +0900

    sigkill

---
+++ restart_sigkill_test.py
@@ -0,0 +1,20 @@
+import sys
+import time
+import signal
+
+# STDOUT을 즉시 flush하도록 설정
+sys.stdout.reconfigure(line_buffering=True)
+
+print("Starting up")
+
+# SIGTERM 시그널을 처리하는 핸들러 정의
+def graceful_shutdown(signum, frame):
+    print("Ignore SIGTERM - continues working")
+
+# SIGTERM 시그널 핸들러 등록
+signal.signal(signal.SIGTERM, graceful_shutdown)
+
+# 무한 루프 실행 (작업 수행을 가장함)
+while True:
+    print("Pretending to do work")
+    time.sleep(3)
